bf4dfa138ebdbd4f09dd460feddccf7251424a79
Sahar Ali <sahar.ali@knowit.se>
2023-10-26 10:36:22 +0200

    changes text

--- HttpExample.cs
+++ HttpExample.cs
@@ -22,7 +22,7 @@ namespace My.Functions
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            response.WriteString("Welcome to Azure Functions! test");
 
             return response;
         }
